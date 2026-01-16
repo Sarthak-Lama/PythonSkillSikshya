e743c6e621033a924f02241dca6542a99b5aeaac
Sarthak Lama <sarthaklama068@gmail.com>
2026-01-16 08:16:39 +0545

    String

--- PythonString/StringBasic.py
+++ PythonString/StringBasic.py
@@ -2,4 +2,4 @@ var1 = "Hello World"
 var2 = "Phython Programming"
 
 print("var1[0]:",var1[0])
-print("var1[1:5]",var2[1:5])
+print("var1[::-1]",var2[::2])   #Start:End:Direction/Step
---
+++ PythonString/updateString.py
@@ -0,0 +1,2 @@
+var1 = 'Hello world'
+print ("Updated string :-", var1[:6]+'Python')
